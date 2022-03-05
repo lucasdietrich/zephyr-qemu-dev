390c990d4b2747e800883684d6004e1907e99249
Lucas Dietrich <ld.adecy@gmail.com>
2022-03-05 14:32:37 +0000

    WIP

--- src/main.c
+++ src/main.c
@@ -7,39 +7,29 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(application, LOG_LEVEL_DBG);
 
-#define UDP_PORT 4242
-#define BUF_SIZE 4096
+#define HOSTNAME "www.google.fr"
 
 void main(void)
 {
-	int fd = socket(AF_INET, SOCK_DGRAM, 0);
-	if (fd < 0) {
-		LOG_ERR("socket failed: %d", fd);
-	}
+	int ret = 0U;
+	struct zsock_addrinfo *ai = NULL;
 
-	struct sockaddr_in sin;
+	for (;;) {		
+		const struct zsock_addrinfo hints = { .ai_family = AF_INET };
+		ret = zsock_getaddrinfo(HOSTNAME, NULL, &hints, &ai);
+		if (ret != 0) {
+			LOG_ERR("failed to resolve hostname err = %d", ret);
+		} else {
+		// convert addrinfo structure ai_addr to string
+			char addr_str[INET_ADDRSTRLEN];
+			zsock_inet_ntop(AF_INET, (const void *)ai->ai_addr, addr_str, sizeof(addr_str));
 
-	sin.sin_family = AF_INET;
-	sin.sin_addr.s_addr = htonl(INADDR_ANY);
-	sin.sin_port = htons(UDP_PORT);
-
-	if (bind(fd, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
-		LOG_ERR("bind failed: %d", errno);
-		close(fd);
-		return;
-	}
-
-	LOG_INF("Listening on UDP port %d", UDP_PORT);
-
-	char buf[BUF_SIZE];
-
-	for (;;) {
-		ssize_t len = recv(fd, buf, BUF_SIZE, 0);
-		if (len < 0) {
-			LOG_ERR("recv failed: %d", errno);
+			// log addr_str
+			LOG_INF("converted addrinfo structure to string: %s", addr_str);
 		}
 
-		LOG_INF("Received %d bytes", len);
-		LOG_HEXDUMP_INF(buf, len, "Received data :");
+		zsock_freeaddrinfo(ai);
+
+		k_sleep(K_SECONDS(25));
 	}
 }
