f124a182c7a73c7babca6ac8fe93add56e4e787d
Lucas Dietrich <ld.adecy@gmail.com>
2022-03-14 19:50:36 +0000

    Resolved AWS hostname and sync time using SNTP

--- aws.md
+++ aws.md
@@ -25,3 +25,24 @@
   - Min heap : 32k
 - Updated timestamp (SNTP)
 - IPv4 addr
+
+## Expected output
+
+```
+[13/14] Linking C executable zephyr/zephyr.elf
+Memory region         Used Size  Region Size  %age Used
+             RAM:      450592 B         3 MB     14.32%
+        IDT_LIST:          0 GB         2 KB      0.00%
+[13/14] To exit from QEMU enter: 'CTRL+a, x'[QEMU] CPU: qemu32,+nx,+pae
+SeaBIOS (version zephyr-v1.0.0-0-g31d4e0e-dirty-20200714_234759-fv-az50-zephyr)
+Booting from ROM..*** Booting Zephyr OS build zephyr-v2.7.1  ***
+
+
+[00:00:00.110,000] <inf> net_config: IPv6 address: 2001:db8::1
+[00:00:00.110,000] <inf> net_config: IPv6 address: 2001:db8::1
+[00:00:00.110,000] <dbg> aws_client.resolve_hostname: Resolving: a31gokdeokxhl8-ats.iot.us-east-1.amazonaws.com
+[00:00:00.140,000] <dbg> aws_client.resolve_hostname: Resolved: 35.175.40.188
+[00:00:00.140,000] <dbg> aws_client.sntp_sync_time: Sending NTP request for current time: 0
+[00:00:00.170,000] <dbg> aws_client.sntp_sync_time: Acquired time from NTP server: 1647287389
+[00:00:00.170,000] <inf> aws_client: UTC time : 2022/03/14 19:49:49
+```
--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <stdio.h>
 
 #include <time.h>
+#include <net/sntp.h>
+#include <posix/time.h>
 
 #include <net/mqtt.h>
 
@@ -17,33 +20,92 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(aws_client, LOG_LEVEL_DBG);
 
-void resolve(const char *hostname)
+void show_time(void)
+{
+	struct timespec tspec;
+	struct tm time_infos;
+
+	clock_gettime(CLOCK_REALTIME, &tspec);
+
+	gmtime_r(&tspec.tv_sec, &time_infos);
+
+	LOG_INF("UTC time : %04d/%02d/%02d %02d:%02d:%02d\n",
+		time_infos.tm_year + 1900, time_infos.tm_mon + 1,
+		time_infos.tm_mday, time_infos.tm_hour,
+		time_infos.tm_min, time_infos.tm_sec);
+}
+
+int sntp_sync_time(void)
+{
+	int rc;
+	struct sntp_time sntp_time;
+        struct timespec tspec;
+
+	LOG_DBG("Sending NTP request for current time: %d", 0);
+
+	rc = sntp_simple("time.google.com", SYS_FOREVER_MS, &sntp_time);
+	if (rc == 0) {
+		tspec.tv_sec = sntp_time.seconds;
+		tspec.tv_nsec = ((uint64_t)sntp_time.fraction * 
+				 (1000LU * 1000LU * 1000LU)) >> 32;
+
+		clock_settime(CLOCK_REALTIME, &tspec);
+		
+		LOG_DBG("Acquired time from NTP server: %u", 
+			(uint32_t)tspec.tv_sec);
+
+	} else {
+		LOG_ERR("Failed to acquire SNTP, code %d\n", rc);
+	}
+	return rc;
+}
+
+void resolve_hostname(const char *hostname)
 {
 	int ret = 0U;
 	struct zsock_addrinfo *ai = NULL;
 
-	const struct zsock_addrinfo hints = { .ai_family = AF_INET, .ai_socktype = SOCK_STREAM, .ai_protocol = 0 };
+	LOG_DBG("Resolving: %s", log_strdup(hostname));
+
+	const struct zsock_addrinfo hints = {
+		.ai_family = AF_INET,
+		.ai_socktype = SOCK_STREAM,
+		.ai_protocol = 0
+	};
 	ret = zsock_getaddrinfo(hostname, "80", &hints, &ai);
 	if (ret != 0) {
-		LOG_ERR("failed to resolve hostname err = %d (errno = %d)", ret, errno);
+		LOG_ERR("failed to resolve hostname err = %d (errno = %d)",
+			ret, errno);
 	} else {
 		char addr_str[INET_ADDRSTRLEN];
-		zsock_inet_ntop(AF_INET, &((struct sockaddr_in*) ai->ai_addr)->sin_addr, addr_str, sizeof(addr_str));
+		zsock_inet_ntop(AF_INET,
+				&((struct sockaddr_in *)ai->ai_addr)->sin_addr,
+				addr_str,
+				sizeof(addr_str));
 
 		// log addr_str
-		LOG_INF("resolve: %s", addr_str);
+		LOG_DBG("Resolved: %s", addr_str);
 	}
 
 	zsock_freeaddrinfo(ai);
+}
+
+void aws_client_thread(void )
+{
+	for (;;) {
+
+	}
 }
 
 void main(void)
 {
 	dns_init_resolver();
 
-	resolve("www.zephyrproject.org");
+	resolve_hostname(CONFIG_CLOUD_AWS_HOSTNAME);
 
-	for (;;) {
-		k_sleep(K_MSEC(1000));
-	}
+	sntp_sync_time();
+
+	show_time();
+
+	aws_client_thread();
 }
